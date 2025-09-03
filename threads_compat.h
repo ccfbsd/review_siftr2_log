565794227845fb743c39de99404d4842b2d153f7
ccfbsd <cc@freebsd.org>
2025-09-03 09:42:32 -0400

    thread code cleanup

--- threads_compat.h
+++ threads_compat.h
@@ -2,34 +2,14 @@
 #define THREADS_COMPAT_H
 
 #include <pthread.h>
-#include <sched.h>     // for sched_yield()
 #include <stdatomic.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <time.h>
 
 // --- Thread Types ---
 typedef pthread_t thrd_t;
-typedef pthread_mutex_t mtx_t;
-typedef pthread_cond_t cnd_t;
-
-// --- Mutex Types ---
-#define mtx_plain     0
-#define mtx_timed     1
-#define mtx_recursive 2
 
 // --- Return Codes ---
 #define thrd_success 0
 #define thrd_error   1
-
-// --- Mutex API ---
-static inline int mtx_init(mtx_t *mtx, int type) {
-    (void)type; // ignoring type for now (could handle recursive if needed)
-    return pthread_mutex_init(mtx, NULL);
-}
-#define mtx_lock(m)    pthread_mutex_lock(m)
-#define mtx_unlock(m)  pthread_mutex_unlock(m)
-#define mtx_destroy(m) pthread_mutex_destroy(m)
 
 // --- Thread API ---
 typedef struct {
@@ -37,14 +17,18 @@ typedef struct {
     void *arg;
 } thrd_start_wrapper_t;
 
-static void *thrd_start_adapter(void *arg) {
+static void *
+thrd_start_adapter(void *arg)
+{
     thrd_start_wrapper_t *wrapper = arg;
     int ret = wrapper->func(wrapper->arg);
     free(wrapper);
     return (void *)(intptr_t)ret; // store int in void*
 }
 
-static inline int thrd_create(thrd_t *thr, int (*func)(void*), void *arg) {
+static inline int
+thrd_create(thrd_t *thr, int (*func)(void*), void *arg)
+{
     thrd_start_wrapper_t *wrapper = malloc(sizeof(*wrapper));
     if (!wrapper) return thrd_error;
     wrapper->func = func;
@@ -53,22 +37,13 @@ static inline int thrd_create(thrd_t *thr, int (*func)(void*), void *arg) {
 }
 #define thrd_join(thr, res) pthread_join(thr, (void**)(res))
 
-// --- Condition Variable API ---
-static inline int cnd_init(cnd_t *cond) {
-    return pthread_cond_init(cond, NULL);
-}
-#define cnd_signal(c)     pthread_cond_signal(c)
-#define cnd_broadcast(c)  pthread_cond_broadcast(c)
-#define cnd_wait(c, m)    pthread_cond_wait(c, m)
-#define cnd_destroy(c)    pthread_cond_destroy(c)
-
 typedef struct {
-    char direction[2];
-    double rel_time;
-    char cwnd[11];
-    char ssthresh[11];
-    char srtt[7];
-    char data_sz[5];
+    char    direction[2];
+    double  rel_time;
+    char    cwnd[11];
+    char    ssthresh[11];
+    char    srtt[7];
+    char    data_sz[5];
 } record_t;
 
 typedef struct {
@@ -78,14 +53,17 @@ typedef struct {
     atomic_bool         done;   // producer sets to true when finished
 } queue_t;
 
-static inline void queue_init(queue_t *q) {
+static inline void
+queue_init(queue_t *q) {
     atomic_init(&q->head, 0);
     atomic_init(&q->tail, 0);
     atomic_init(&q->done, false);
 }
 
 // Returns false if the buffer is full (no push performed)
-static inline bool queue_push(queue_t *q, const record_t *rec) {
+static inline bool
+queue_push(queue_t *q, const record_t *rec)
+{
     size_t tail = atomic_load_explicit(&q->tail, memory_order_relaxed);
     size_t head = atomic_load_explicit(&q->head, memory_order_acquire);
 
@@ -102,7 +80,9 @@ static inline bool queue_push(queue_t *q, const record_t *rec) {
 }
 
 // Returns false if the buffer is empty (no pop performed)
-static inline bool queue_pop(queue_t *q, record_t *rec) {
+static inline bool
+queue_pop(queue_t *q, record_t *rec)
+{
     size_t head = atomic_load_explicit(&q->head, memory_order_relaxed);
     size_t tail = atomic_load_explicit(&q->tail, memory_order_acquire);
 
@@ -116,15 +96,21 @@ static inline bool queue_pop(queue_t *q, record_t *rec) {
     return true;
 }
 
-static inline void queue_set_done(queue_t *q) {
+static inline void
+queue_set_done(queue_t *q)
+{
     atomic_store_explicit(&q->done, true, memory_order_release);
 }
 
-static inline bool queue_is_done(queue_t *q) {
+static inline bool
+queue_is_done(queue_t *q)
+{
     return atomic_load_explicit(&q->done, memory_order_acquire);
 }
 
-static inline bool queue_is_empty(queue_t *q) {
+static inline bool
+queue_is_empty(queue_t *q)
+{
     size_t head = atomic_load_explicit(&q->head, memory_order_relaxed);
     size_t tail = atomic_load_explicit(&q->tail, memory_order_relaxed);
     return head == tail;
